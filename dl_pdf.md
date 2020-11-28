7a09f1974c991b579ee75081100264c401650951
Frederic GAURAT <fgaurat@eolem.com>
2020-11-28 17:06:33 +0100

    add win and parallel download

--- dl_pdf.md
+++ dl_pdf.md
@@ -1,6 +1,7 @@
 https://cdn.filestackcontent.com/UPqbkTIETnGaQJa1nqnG?dl=true
 https://cdn.filestackcontent.com/fLQxwCxESd6ld3Eu6LqB?dl=true
 https://cdn.filestackcontent.com/IqqTIN7HRuWYjdOkIJ5M?dl=true
+https://cdn.filestackcontent.com/yWc2KmlTRoSCtNg0GMr6?dl=true
 https://cdn.filestackcontent.com/__URL__?dl=true
 
 
@@ -9,3 +10,12 @@ IqqTIN7HRuWYjdOkIJ5M
 
 fLQxwCxESd6ld3Eu6LqB
 IqqTIN7HRuWYjdOkIJ5M
+
+
+youtube-dl -o "00-video-%(title)s" https://fast.wistia.net/embed/iframe/3zlt3ji1rn?videoFoam=true &
+youtube-dl -o "01-video-%(title)s" https://fast.wistia.net/embed/iframe/voylzsw34c?videoFoam=true &
+youtube-dl -o "02-video-%(title)s" https://fast.wistia.net/embed/iframe/u1393bhs7h?videoFoam=true &
+youtube-dl -o "03-video-%(title)s" https://fast.wistia.net/embed/iframe/j9lc6i369d?videoFoam=true
+youtube-dl -o "04-video-%(title)s" https://fast.wistia.net/embed/iframe/422mfq6nym?videoFoam=true
+youtube-dl -o "05-video-%(title)s" https://fast.wistia.net/embed/iframe/pidp9u8mz8?videoFoam=true
+youtube-dl -o "06-video-%(title)s" https://fast.wistia.net/embed/iframe/dj20wpkzd6?videoFoam=true
--- public/index.js
+++ public/index.js
@@ -12,6 +12,7 @@ window.onload = () => {
         document.getElementById('results').innerHTML=""
         document.getElementById('results_area').value=""
         document.getElementById('yt_results_area').value=""
+        document.getElementById('yt_results_area_win').value=""
 
     }
 
@@ -22,6 +23,7 @@ window.onload = () => {
         const results_area = document.getElementById('results_area');
 
         const yt_results_area = document.getElementById('yt_results_area');
+        const yt_results_area_win = document.getElementById('yt_results_area_win');
         const project_name = document.getElementById('project_name');
 
         results.innerHTML=""
@@ -43,7 +45,8 @@ window.onload = () => {
             results.innerHTML+= a
             results_area.value+=video_url+"\n"
 
-            yt_results_area.value+=`youtube-dl -o "${i.toString().padStart(2, "0")}-${project_name.value.trim()}-%(title)s" ${video_url}\n`
+            yt_results_area.value+=`youtube-dl -o "${i.toString().padStart(2, "0")}-${project_name.value.trim()}-%(title)s" ${video_url} &\n`
+            yt_results_area_win.value+=`youtube-dl -o "${i.toString().padStart(2, "0")}-${project_name.value.trim()}-%(title)s" ${video_url}\n`
 
         }
         
